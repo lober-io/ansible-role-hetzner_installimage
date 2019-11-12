a2893bac0c59413797ae6cdbae9be9b7513d2ade
Dirk Pahl <dirk.pahl@web.de>
2019-11-12 14:47:19 +0100

    Add testcase for raid1 though not for autodetect

---
+++ molecule/raid1/tests/test_raid1.py
@@ -0,0 +1,20 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_autosetupfile_exists(host):
+    f = host.file('/autosetup')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+    assert f.contains('SWRAID 1')
+    assert f.contains('SWRAIDLEVEL 1')
+    assert f.contains('DRIVE1')
+    assert f.contains('DRIVE2')
